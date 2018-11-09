1b38759c3b4c3414ef46e43efcb2ffe8adf22178
Ben Jacobs <ben@notion.com>
2018-11-09 13:52:44 -0700

    reduce cognitive complexity

--- lib/recoverable/base.rb
+++ lib/recoverable/base.rb
@@ -36,17 +36,34 @@ module Recoverable
 
   def fetch_handler_args(args, instance, req_params, error)
     custom_handler_args = {error: error}
-    local_args          = args.map(&:to_a).flatten(1).to_h
-    ivs                 = instance.instance_values.keys.map(&:to_sym)
+    local_args          = fetch_local_args(args)
+    evaluateables       = evaluateable_keys_on(instance)
+    generate_custom_handler_args(req_params: req_params, evaluateables: evaluateables, local_args: local_args, instance: instance, custom_handler_args: custom_handler_args)
+  end
 
+  def generate_custom_handler_args(req_params:, evaluateables:, local_args:, instance:, custom_handler_args:)
     req_params.each do |key|
-      custom_handler_args[key] ||= instance.send(:eval, key.to_s)  if (ivs + instance.send(:public_methods) + instance.send(:local_variables)).include?(key)
+      custom_handler_args[key] ||= instance.send(:eval, key.to_s)  if evaluateables.include?(key)
       custom_handler_args[key] ||= local_args[key] if local_args.keys.include?(key)
     end
 
     custom_handler_args
   end
 
+  def fetch_instance_variables(instance)
+    instance.instance_values.keys.map(&:to_sym)
+  end
+
+  def fetch_local_args(args)
+    args.map(&:to_a).flatten(1).to_h
+  end
+
+  def evaluateable_keys_on(instance)
+    fetch_instance_variables(instance) +
+    instance.send(:public_methods) +
+    instance.send(:local_variables)
+  end
+  
 end
 
 
